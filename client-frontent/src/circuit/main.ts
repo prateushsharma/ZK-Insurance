338c8ab8914f50876a539b46bc42d767dd69f040
Noor <noormohammedb@protonmail.com>
2024-12-07 19:52:54 +0530

    insurance matching circuit

--- client-frontent/src/circuit/main.ts
+++ client-frontent/src/circuit/main.ts
@@ -2,38 +2,69 @@
 // demonstrate that you can split up your summon code like this.
 // import find_matching_insurer from './insurance.ts';
 
-export default function main(a: PersonHealthProfile, b: [InsurancePolicyProfile]): InsurancePolicyProfile {
-  // return find_matching_insurer(a, b) ? a : b;
-  find_matching_insurer(a, b); 
+export default function main(
+  a: PersonHealthProfile,
+  b: [InsurancePolicyProfile],
+): InsurancePolicyProfile[] {
+  let matched_insurers: InsurancePolicyProfile[] = [];
 
-  return b[0]
-}
-function find_matching_insurer(a: PersonHealthProfile, b: [InsurancePolicyProfile]): boolean {
-  return false;
+  for (let i = 0; i < b.length; i++) {
+    if (find_matching_insurer(a, b[i])) {
+      matched_insurers.push(b[i]);
+    }
+  }
+
+  return matched_insurers;
 }
 
+function find_matching_insurer(
+  a: PersonHealthProfile,
+  b: InsurancePolicyProfile,
+): boolean {
+  if (a.age < b.age.min || a.age > b.age.max) {
+    return false;
+  } else if (a.height < b.height.min || a.height > b.height.max) {
+    return false;
+  } else if (a.weight < b.weight.min || a.weight > b.weight.max) {
+    return false;
+  } else if (!b.bloodGroup.includes(a.bloodGroup)) {
+    return false;
+  } else if (!b.gender.includes(a.gender)) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 interface InsurancePolicyProfile {
   age: {
     min: number;
     max: number;
   };
-  gender: "male" | "female" | "other";
-  bloodGroup: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
-  exercise: {
-    frequency: "daily" | "weekly" | "monthly" | "rarely";
-    duration: number; // in minutes
+  height: {
+    min: number;
+    max: number;
   };
-  medicalConditions: [string];
+  weight: {
+    min: number;
+    max: number;
+  };
+  gender: [string];
+  bloodGroup: [string];
+  // bloodGroup: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  // medicalConditions: [string];
 }
 
 interface PersonHealthProfile {
   age: number;
-  gender: "male" | "female" | "other";
-  bloodGroup: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
-  exercise: {
-    frequency: "daily" | "weekly" | "monthly" | "rarely";
-    duration: number; // in minutes
-  };
-  medicalConditions: [string];
+  height: number;
+  weight: number;
+  gender: string;
+  bloodGroup: string;
+  // medicalConditions: [string];
+  // bloodGroup: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  // exercise: {
+  //   frequency: "daily" | "weekly" | "monthly" | "rarely";
+  //   duration: number; // in minutes
+  // };
 }
